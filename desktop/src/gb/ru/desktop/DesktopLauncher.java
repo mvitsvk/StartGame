c4135c81cd9e7fdd6c1e20e9ae574420559c95c3
Vitaliy <mvit_svk@mail.ru>
2021-10-21 00:44:33 +0300

    dz1

--- desktop/src/gb/ru/desktop/DesktopLauncher.java
+++ desktop/src/gb/ru/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import gb.ru.StarGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width=400;
+		config.height=800;
 		new LwjglApplication(new StarGame(), config);
 	}
 }
